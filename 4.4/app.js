699efbde09b7aa3d6d6a16fc15372cf6c14253a7
Andrey <65175184+DoroshukA@users.noreply.github.com>
2020-08-03 12:18:36 +0300

    4

---
+++ 4.4/app.js
@@ -0,0 +1,27 @@
+// const express = require("express");
+ 
+// const app = express();
+ 
+// app.use(express.static(__dirname + "/public"));
+ 
+// app.use("/", function(request, response){
+     
+//     response.send("<h1>Главная страница</h1>");
+// });
+ 
+// app.listen(3000);
+
+//======================================
+
+const express = require("express");
+ 
+const app = express();
+ 
+app.use("/static", express.static(__dirname + "/public")); //http://localhost:3000/static/about.html
+ 
+app.use("/", function(request, response){
+     
+    response.send("<h1>Главная страница</h1>");
+});
+ 
+app.listen(3000);
