fc396b373f0c9d5e3dd08cc85f1652370b3bbe5e
Andrey <65175184+DoroshukA@users.noreply.github.com>
2020-08-03 12:02:15 +0300

    2.9

---
+++ 2.9/app.js
@@ -0,0 +1,60 @@
+// const Emitter = require("events");
+// let emitter = new Emitter();
+// let eventName = "greet";
+// emitter.on(eventName, function(){
+//     console.log("Hello all!");
+// });
+// emitter.on(eventName, function(){
+//     console.log("Привет!");
+// });
+// emitter.emit(eventName);
+
+//==========================================
+
+// const Emitter = require("events");
+// let emitter = new Emitter();
+// let eventName = "greet";
+// emitter.on(eventName, function(data){
+//     console.log(data);
+// });
+// emitter.emit(eventName, "Привет пир!");
+
+//===========================================
+
+// const util = require("util");
+// const EventEmitter = require("events");
+ 
+// function User(){
+// }
+// util.inherits(User, EventEmitter);
+ 
+// let eventName = "greet";
+// User.prototype.sayHi = function(data){
+//     this.emit(eventName, data);
+// }
+// let user = new User();
+// // добавляем к объекту user обработку события "greet"
+// user.on(eventName, function(data){
+//     console.log(data);
+// });
+ 
+// user.sayHi("Мне нужна твоя одежда...");
+
+
+const EventEmitter = require("events");
+  
+let eventName = "greet";
+ 
+class User extends EventEmitter {
+    sayHi(data) {
+        this.emit(eventName, data);
+    }
+}
+ 
+let user = new User();
+// добавляем к объекту user обработку события "greet"
+user.on(eventName, function(data){
+    console.log(data);
+});
+  
+user.sayHi("Мне нужна твоя одежда...");
